3afb1c20ab6a282ed999eda62a3760d12217b623
Rouidfaida <rouid.faida@yahoo.fr>
2024-03-01 16:43:17 +0100

    reactsite

--- back/server.js
+++ back/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/api2/visits', (req, res) => {
   const visitData = req.body;
