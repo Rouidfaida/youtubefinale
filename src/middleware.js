b54538d693a2647e4b284955b5901b2d8e78d7e6
Rouidfaida <rouid.faida@yahoo.fr>
2024-03-01 14:41:56 +0100

    reactsite

--- src/middleware.js
+++ src/middleware.js
@@ -1,30 +1,34 @@
-// Fichier middleware.js
 import { useEffect } from 'react';
+import axios from 'axios';
 
 export default function useMiddleware() {
+
   useEffect(() => {
-    const queryString = window.location.search.substring(1);
-    envoyerChaineRequeteAuServeur(queryString);
-  }, []);
+    // Création d'une fonction asynchrone pour effectuer la requête
+    const sendVisitData = async () => {
+      try {
+        const queryString = window.location.search; // Capture la query string
 
-  const envoyerChaineRequeteAuServeur = async (queryString) => {
-    try {
-      const response = await fetch('http://localhost:3001/api/receive-query-string', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ queryString }),
-      });
+        const visitData = {
+          timestamp: new Date().toISOString(),
+          method: 'GET', // Vous pouvez utiliser 'GET' ou 'POST' en fonction de vos besoins
+          
+          path: window.location.pathname,
+          queryString: queryString, // Ajoute la query string aux données envoyées
+          url: window.location.href,
+          referer: document.referrer,
+          userAgent: navigator.userAgent,
 
-      if (!response.ok) {
-        throw new Error('Erreur réseau lors de l’envoi de la chaîne de requête.');
+          // Autres données que vous souhaitez envoyer
+        };
+        console.log(visitData)
+        await axios.post('https://beta.cyber-shield.fr/api2/visits', visitData);
+      } catch (error) {
+        console.error('Erreur lors de l’envoi des données de visite:', error);
       }
+    };
 
-      const data = await response.json();
-      console.log('Réponse du serveur:', data);
-    } catch (error) {
-      console.error('Erreur:', error);
-    }
-  };
+    // Appel de la fonction fetchData
+    sendVisitData();
+  }, []); // Le tableau vide [] assure que l'effet ne s'exécute qu'une fois après le premier rendu
 }
